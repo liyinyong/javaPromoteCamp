3b72f2a2ee6e8ee2828af23beef6bfceddf83b89
liyinyong <liyinyong@ad61v1.com>
2021-12-08 23:24:34 +0800

    第五周作业

--- five-week-homework/LocalStarter/src/main/java/com/example/localstarter/config/LocalProperties.java
+++ five-week-homework/LocalStarter/src/main/java/com/example/localstarter/config/LocalProperties.java
@@ -3,9 +3,24 @@ package com.example.localstarter.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "local.properties")
-@Data
+@ConfigurationProperties(prefix = "local")
 public class LocalProperties {
     private Integer id;
     private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- five-week-homework/LocalStarter/src/main/java/com/example/localstarter/config/LocalStarterConfiguration.java
+++ five-week-homework/LocalStarter/src/main/java/com/example/localstarter/config/LocalStarterConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({LocalProperties.class})
+@EnableConfigurationProperties(LocalProperties.class)
 @ConditionalOnClass(LocalProperties.class)
 public class LocalStarterConfiguration {
 
