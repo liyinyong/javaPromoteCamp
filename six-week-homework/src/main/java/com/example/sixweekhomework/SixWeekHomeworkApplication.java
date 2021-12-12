901e86755c51018f746ce7e4731f90798826c9cc
liyinyong <liyinyong@ad61v1.com>
2021-12-12 20:07:54 +0800

    提交作业

---
+++ six-week-homework/src/main/java/com/example/sixweekhomework/SixWeekHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.sixweekhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixWeekHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixWeekHomeworkApplication.class, args);
+    }
+
+}
---
+++ six-week-homework/src/test/java/com/example/sixweekhomework/SixWeekHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sixweekhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixWeekHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
