7c69afef4b93a545d939f3e667b6f23b36dc4b1d
liyinyong <liyinyong@ad61v1.com>
2021-12-27 00:04:01 +0800

    先完成一部分

---
+++ eighth-week-homework/xa-server2/src/main/java/com/example/xaserver2/XaServer2Application.java
@@ -0,0 +1,13 @@
+package com.example.xaserver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XaServer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XaServer2Application.class, args);
+    }
+
+}
