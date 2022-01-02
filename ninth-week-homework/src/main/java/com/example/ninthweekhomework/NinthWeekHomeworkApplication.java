08570c7aa99914aa167e8a7786a13bba67710137
liyinyong <liyinyong@ad61v1.com>
2022-01-02 19:47:49 +0800

    第九周作业

---
+++ ninth-week-homework/src/main/java/com/example/ninthweekhomework/NinthWeekHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.ninthweekhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinthWeekHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NinthWeekHomeworkApplication.class, args);
+    }
+
+}
---
+++ ninth-week-homework/src/test/java/com/example/ninthweekhomework/NinthWeekHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ninthweekhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinthWeekHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
