d9f28dd94fae0c21fc0a9ee3cc2787b850a75627
liyinyong <liyinyong@ad61v1.com>
2022-01-04 19:59:22 +0800

    第九周作业

--- ninth-week-homework/dubbo-hmily-tcc/tcc-server-1/src/test/java/com/example/tccserver1/TccServer1ApplicationTests.java
+++ ninth-week-homework/dubbo-hmily-tcc/tcc-server-1/src/test/java/com/example/tccserver1/TccServer1ApplicationTests.java
@@ -1,12 +1,10 @@
 package com.example.tccserver1;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TccServer1ApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
--- ninth-week-homework/dubbo-hmily-tcc/tcc-server-2/src/test/java/com/example/tccserver2/TccServer2ApplicationTests.java
+++ ninth-week-homework/dubbo-hmily-tcc/tcc-server-2/src/test/java/com/example/tccserver2/TccServer2ApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.tccserver2;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TccServer2ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
