f016ac10c3cf93042804846095d8d9511ecf4023
liyinyong <liyinyong@ad61v1.com>
2022-01-23 23:22:32 +0800

    作业

---
+++ twelveth-week-homework/src/main/java/com/example/twelvethweekhomework/TwelvethWeekHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.twelvethweekhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelvethWeekHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwelvethWeekHomeworkApplication.class, args);
+    }
+
+}
---
+++ twelveth-week-homework/src/test/java/com/example/twelvethweekhomework/TwelvethWeekHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twelvethweekhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwelvethWeekHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
