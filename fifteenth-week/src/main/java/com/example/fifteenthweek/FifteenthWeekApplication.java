1f0c0fd51c5dbba7b4ef1c4ad18ef3f9e85f12fd
liyinyong <liyinyong@ad61v1.com>
2022-02-13 19:08:03 +0800

    15周

---
+++ fifteenth-week/src/main/java/com/example/fifteenthweek/FifteenthWeekApplication.java
@@ -0,0 +1,13 @@
+package com.example.fifteenthweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifteenthWeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifteenthWeekApplication.class, args);
+    }
+
+}
